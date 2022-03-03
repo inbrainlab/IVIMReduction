411b43dab43f132dbaf7f2c9d9d7c835e90d2fcb
lcscosta <lucasdacosta@usp.br>
2022-03-03 20:49:08 -0300

    Refactoring code
    - Added Setup.py
    - Remake the folder architecture
    - Backup in old/ folder

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ivimreduction",
+    version="0.0.1",
+    author="Lucas da Costa",
+    author_email="lucasdacosta@usp.br",
+    description="A package for simulation of b-values reduction",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
---
+++ src/ivimreduction/__init__.py
@@ -0,0 +1,6 @@
+# Added all functions from archives
+from . reduction import hello
+#from . divide import division
+#from . multiply import multiply
+#from . subtract import subtract
+#from .adv.sqrt import squareroot
