81c98d0c9a33e680cfa8ad91540362b598ea80da
lcscosta <lucasdacosta@usp.br>
2022-03-08 21:49:29 -0300

    Added class LinearReduction and class variables and first methods.
    - Included class variables:
      bvals, data and n_reduction
      bvals_best and rrmse_best
    
    - Methods:
      rrmse_calculator and rrmse_mean

--- src/ivimreduction/reduction.py
+++ src/ivimreduction/reduction.py
@@ -1,2 +1,41 @@
 def hello():
     print("Hello World")
+
+
+class LinearReduction:
+    """ Class to calculate the linear reduction with respect fitting method """
+    # Class Variables for inviduals objects
+    def __init__(self, bvals, data, n_reduction, fitting_method):
+        self.bvals = bvals
+        self.data = data
+        self.n_reduction = n_reduction
+        self.fitting_method = fitting_method
+
+    # Class Variables for all objects
+    bvals_best = [] # (bvals) x n_reduction
+    rrmse_best = [] # 1 x n_reduction
+
+    # Methods
+    def rrmse_calculator(data_array, bvals, model, estimated_params):
+        """ A method to calculate the
+
+        data_array - np.array with 1 x len(bvals)
+        bvals - np.array
+        model - function(bvals, estimated_params)
+            (D, D*, f, bvals) -> [1xlen(bvals)]
+        estimated_params - np.array with 1x3 (D, D*, f)
+        """
+
+        return 0
+
+    def mean_rrmse(data, bvals, model, estimated_params):
+        """ A method to calculate the
+
+        data_array - np.array with 1 x len(bvals)
+        bvals - np.array
+        model - function(bvals, estimated_params)
+            (D, D*, f, bvals) -> [1xlen(bvals)]
+        estimated_params - np.array with 1x3 (D, D*, f)
+        """
+
+        return 0
